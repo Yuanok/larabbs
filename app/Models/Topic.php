214fa2a8aee9a21fa2d884b37fd46fdda15a8dff
Yuanok2 <382547456@qq.com>
2019-05-23 17:50:10 +0800

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,12 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        $this->belongsTo(Category::class);
+    }
 }
