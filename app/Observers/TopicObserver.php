b574d5e4094943199116da73e3dc7e5d882930a6
Yuanok2 <382547456@qq.com>
2019-05-28 11:41:26 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -21,12 +22,17 @@ class TopicObserver
     }
 
     public function saving(Topic $topic){
+        //xss过滤
         $topic->body = clean($topic->body,'user_topic_body');
-
+        //生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    
+    }
 
+    public function saved(Topic $topic){
         if(!$topic->slug){
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
